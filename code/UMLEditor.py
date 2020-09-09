31c096c5a8f2719969b8956e857e33c8037b7e2a
Amy Burnett <16aburnett@gmail.com>
2020-09-09 15:32:50 -0400

    Added usages and descriptions for each of our valid commands

--- code/UMLEditor.py
+++ code/UMLEditor.py
@@ -17,6 +17,8 @@ import UMLRelationship
 ##########################################################################
 # Constants 
 
+# Data structure for storing all of the valid commands
+#   and any extra information to give to the user 
 COMMANDS = {
     "help" : [
         {
@@ -28,14 +30,86 @@ COMMANDS = {
             "desc" : "lists the usage for <command_name>"
         }
     ],
+    "exit" : [
+        {
+            "usage" : "exit",
+            "desc" : "exits the program"
+        }
+    ],
     "create_class" : [
         {
             "usage" : "create_class <class_name>",
-            "desc" : "creates a class with <class_name> as the name"
+            "desc" : "creates a class named <class_name> and adds it to the model"
+        }
+    ],
+    "rename_class" : [
+        {
+            "usage" : "rename_class <old_name> <new_name>",
+            "desc" : "renames the class <old_name> with the <new_name>"
+        }
+    ],
+    "delete_class" : [
+        {
+            "usage" : "delete_class <class_name>",
+            "desc" : "deletes the class with the name <class_name> from the model"
+        }
+    ],
+    "create_attribute" : [
+        {
+            "usage" : "create_attribute <class_name> <attrib_name>",
+            "desc" : "creates an attribute named <class_name> and adds it to the given class"
+        }
+    ],
+    "rename_attribute" : [
+        {
+            "usage" : "rename_attribute <class_name> <old_name> <new_name>",
+            "desc" : "renames the attribute <old_name> with the <new_name> for the class, <class_name>"
+        }
+    ],
+    "delete_attribute" : [
+        {
+            "usage" : "delete_attribute <class_name> <attrib_name>",
+            "desc" : "deletes the attribute from the class"
+        }
+    ],
+    "create_relationship" : [
+        {
+            "usage" : "create_relationship <class1> <class2>",
+            "desc" : "creates a relationship between <class1> and <class2>"
+        }
+    ],
+    "delete_relationship" : [
+        {
+            "usage" : "delete_relationship <class1> <class2>",
+            "desc" : "removes the relationship between <class1> and <class2>"
+        }
+    ],
+    "save_model" : [
+        {
+            "usage" : "save_model <filename>",
+            "desc" : "saves the UMLModel into <filename> as JSON"
+        }
+    ],
+    "load_model" : [
+        {
+            "usage" : "load_model <filename>",
+            "desc" : "loads the UMLModel from <filename> and uses it as the working UMLModel"
+        }
+    ],
+    "list_class" : [
+        {
+            "usage" : "list_class <class_name>",
+            "desc" : "prints all of the attributes for a given class"
+        }
+    ],
+    "list_relationships" : [
+        {
+            "usage" : "list_relationships",
+            "desc" : "prints out the classes that each class relate to"
         }
     ]
-}
 
+}
 
 ##########################################################################
 
