88710f2fb0ba1a463ae5a4684b40aeb0176965fe
adjusic <adjusic@protonmail.ch>
2020-10-04 04:12:07 -0400

    Fixed card display area & draggable feature & added calculation for card coordinates

--- code/WebApp/static/js/myscripts.js
+++ code/WebApp/static/js/myscripts.js
@@ -1,9 +1,25 @@
+// Runs automatically when web application is executed
 $(function() {
-    $(".card").draggable({
-        cursor: "crosshair",
-        opacity: 0.5,
-        containment: "parent"
-    });
-
-    $(".card").addClass("shadow");
+    draggable("#cardId1");
+    draggable("#cardId2");
+    draggable("#cardId3");
 });
+
+// Gives class card the ability to be draggable and calculates
+// the coordinates of the class card on the display  
+function draggable(card) {
+    $(card).draggable({
+    cursor: "crosshair",
+    opacity: 0.5,
+    containment: "parent",
+        // Calculates x and y coordinates of class card as you drag it
+        drag: function() {
+            var $this = $(this);
+            var thisPos = $this.position();
+            var parentPos = $this.parent().position();
+
+            var x = thisPos.left - parentPos.left;
+            var y = thisPos.top - parentPos.top;    
+        }
+    });
+}
