93595d67d293ec826f2afc01ffeac720327251ed
Adisa Jusić <adjusic@protonmail.ch>
2020-10-06 23:36:09 -0400

    Implemented the delete button for class cards (#129)
    * Implemented the delete button for class cards
    
    * changed .click() to .on(...)

--- code/WebApp/static/js/myscripts.js
+++ code/WebApp/static/js/myscripts.js
@@ -1,22 +1,44 @@
 // Runs automatically when web application is executed
 $(function() {
+    classCardBtns();
 });
 
 // Gives class card the ability to be draggable and calculates
 // the coordinates of the class card on the display  
 function draggable(card) {
     $(card).draggable({
-    cursor: "crosshair",
-    opacity: 0.5,
-    containment: "parent",
-        // Calculates x and y coordinates of class card as you drag it
-        drag: function() {
-            var $this = $(this);
-            var thisPos = $this.position();
-            var parentPos = $this.parent().position();
+        stack: ".card",
+        cursor: "crosshair",
+        opacity: 0.5,
+        containment: "parent",
+        snap: true
+    });
+}
 
-            var x = thisPos.left - parentPos.left;
-            var y = thisPos.top - parentPos.top;    
-        }
+// Functionality for the class card buttons
+function classCardBtns() {
+    var getId;
+    // Gets the unique ID of a class card
+    $('.card-footer').on('click', '.getID', function () {
+        getId = $($(this).closest('.card')).attr("id");
+    });
+
+    // Allows us to delete a specific class card on click of its delete button
+    $('.delCard').click(function () {
+        // removes the class card from dashboard along with all its content
+        $("#" + getId).remove("#" + getId);
+        // closes the delete modal that varifies if user wants to delete the selected class card
+        $("#deleteModal").modal('hide');
+        // displays a confirmation modal informing user the class card has been deleted
+        $("#confirmModal").modal('show');
+    });
+
+    // Allows us to edit a specific class card on click of its edit button
+    $('.card-footer').on('click', '.editCard', function () {
+        // Get all the information pertaining to the class we wish to edit using the unique class ID
+
+        // Preload the information we got into the appropriate textboxes of the edit Class Modal
+
+        alert("Edit class: #" + getId);
     });
 }
