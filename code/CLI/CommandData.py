3fb44c95ea06028f127726a1cb2a4955a0351eaf
DavitPetro <64999212+DavitPetro@users.noreply.github.com>
2020-10-20 17:11:31 -0400

    Changed ordering and output of 'help' command
    - Commands in CommandData.py have been reordered.
    - The 'help' command now outputs the commands in that order and groups them into categories.

--- code/CLI/CommandData.py
+++ code/CLI/CommandData.py
@@ -13,56 +13,14 @@ from command.command import UndoableCLICommand, CLICommand
 # Data structure for storing all of the valid commands
 #   and any extra information to give to the user 
 COMMANDS = {
-    "help" : [
-        {
-            "usage" : "help", 
-            "desc" : "lists all possible commands",
-            "command" : CLICommand,
-            "function" : CLIEditor.print_help_message,
-            "num_arguments" : 0
-        },
-        {
-            "usage" : "help <command_name>", 
-            "desc" : "lists the usage for <command_name>",
-            "command" : CLICommand,
-            "function" : CLIEditor.print_help_message,
-            "num_arguments" : 1
-        }
-    ],
-    "undo" : [
-        {
-            "usage" : "undo", 
-            "desc" : "undoes the last undoable command (if there was one)",
-            "command" : CLICommand,
-            "function" : CLIEditor.undo,
-            "num_arguments" : 0
-        }
-    ],
-    "redo" : [
-        {
-            "usage" : "redo", 
-            "desc" : "redoes the last undo'd command (if there was one)",
-            "command" : CLICommand,
-            "function" : CLIEditor.redo,
-            "num_arguments" : 0
-        }
-    ],
-    "exit" : [
-        {
-            "usage" : "exit",
-            "desc" : "exits the program",
-            "command" : CLICommand,
-            "function" : CLIEditor.prompt_exit,
-            "num_arguments" : 0
-        }
-    ],
     "create_class" : [
         {
             "usage" : "create_class <class_name>",
             "desc" : "creates a class named <class_name> and adds it to the model",
             "command" : UndoableCLICommand,
             "function" : UMLModel.create_class,
-            "num_arguments" : 1 
+            "num_arguments" : 1, 
+            "skip_line" : False
         }
     ],
     "rename_class" : [
@@ -71,7 +29,8 @@ COMMANDS = {
             "desc" : "renames the class <old_name> with the <new_name>",
             "command" : UndoableCLICommand,
             "function" : UMLModel.rename_class,
-            "num_arguments" : 2
+            "num_arguments" : 2,
+            "skip_line" : False
         }
     ],
     "delete_class" : [
@@ -80,113 +39,8 @@ COMMANDS = {
             "desc" : "deletes the class with the name <class_name> from the model",
             "command" : UndoableCLICommand,
             "function" : UMLModel.delete_class,
-            "num_arguments" : 1
-        }
-    ],
-    "create_field" : [
-        {
-            "usage" : "create_field <class_name> <visibility> <field_type> <field_name>",
-            "desc" : "creates a field named <field_name> of type <field_type> and adds it to the given class \n <visibility> determines if the field is public/private,",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.create_field,
-            "num_arguments" : 4
-        }
-    ],
-    "rename_field" : [
-        {
-            "usage" : "rename_field <class_name> <old_name> <new_name>",
-            "desc" : "renames the attribute <old_name> with the <new_name> for the class, <class_name>",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.rename_field,
-            "num_arguments" : 3
-        }
-    ],
-    "delete_field" : [
-        {
-            "usage" : "delete_field <class_name> <attrib_name>",
-            "desc" : "deletes the field from the class",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.delete_field,
-            "num_arguments" : 2
-        }
-    ],
-    "move_up_field" : [
-        {
-            "usage" : "move_up_field <class_name> <field_name>",
-            "desc" : "moves field up one space in a list of fields",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.move_up_field,
-            "num_arguments" : 2
-        }
-    ],
-    "move_down_field" : [
-        {
-            "usage" : "move_down_field <class_name> <field_name>",
-            "desc" : "moves field down one space in a list of foe;ds",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.move_down_field,
-            "num_arguments" : 2
-        }
-    ],
-    "create_relationship" : [
-        {
-            "usage" : "create_relationship <relationship_type> <class1> <class2>",
-            "desc" : "creates a relationship between <class1> and <class2>\n<relationship_type> should be one of \n{inheritance, generalization, aggregation, composition}",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.create_relationship,
-            "num_arguments" : 3
-        }
-    ],
-    "delete_relationship" : [
-        {
-            "usage" : "delete_relationship <class1> <class2>",
-            "desc" : "removes the relationship between <class1> and <class2>",
-            "command" : UndoableCLICommand,
-            "function" : UMLModel.delete_relationship,
-            "num_arguments" : 2
-        }
-    ],
-    "move_up_relationship" : [
-       {
-        "usage" : "move_up_relationship <class_name1> <class_name2>",
-        "desc" : "moves relationship up one space in a list of the first specified class's relationships",
-        "function" : UMLModel.move_up_relationship,
-        "num_arguments" : 2
-       }
-    ],
-    "move_down_relationship" : [
-       {
-        "usage" : "move_down_relationship <class_name1> <class_name2>",
-        "desc" : "moves relationship down one space in a list of the first specified class's relationships",
-        "function" : UMLModel.move_down_relationship,
-        "num_arguments" : 2
-       }
-    ],
-    "save_model" : [
-        {
-            "usage" : "save_model <filename>",
-            "desc" : "saves the UMLModel into <filename> as JSON",
-            "command" : CLICommand,
-            "function" : UMLModel.save_model,
-            "num_arguments" : 1
-        }
-    ],
-    "load_model" : [
-        {
-            "usage" : "load_model <filename>",
-            "desc" : "loads the UMLModel from <filename> and uses it as the working UMLModel",
-            "command" : CLICommand,
-            "function" : UMLModel.load_model,
-            "num_arguments" : 1
-        }
-    ],
-    "list_classes" : [
-        {
-            "usage" : "list_classes",
-            "desc" : "prints out the name of all classes in the model",
-            "command" : CLICommand,
-            "function" : UMLModel.list_classes,
-            "num_arguments" : 0
+            "num_arguments" : 1,
+            "skip_line" : False
         }
     ],
     "list_class" : [
@@ -195,32 +49,78 @@ COMMANDS = {
             "desc" : "prints out all components of the <class_name>",
             "command" : CLICommand,
             "function" : UMLModel.list_class,
-            "num_arguments" : 1
+            "num_arguments" : 1,
+            "skip_line" : False
         }
     ],
+    "list_classes" : [
+        {
+            "usage" : "list_classes",
+            "desc" : "prints out the name of all classes in the model",
+            "command" : CLICommand,
+            "function" : UMLModel.list_classes,
+            "num_arguments" : 0,
+            "skip_line" : True
+        }
+    ],
+    "create_field" : [
+        {
+            "usage" : "create_field <class_name> <visibility> <field_type> <field_name>",
+            "desc" : "creates a field named <field_name> of type <field_type> and adds it to the given class \n <visibility> determines if the field is public/private,",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.create_field,
+            "num_arguments" : 4,
+            "skip_line" : False
+        }
+    ],
+    "rename_field" : [
+        {
+            "usage" : "rename_field <class_name> <old_name> <new_name>",
+            "desc" : "renames the attribute <old_name> with the <new_name> for the class, <class_name>",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.rename_field,
+            "num_arguments" : 3,
+            "skip_line" : False
+        }
+    ],
+    "delete_field" : [
+        {
+            "usage" : "delete_field <class_name> <attrib_name>",
+            "desc" : "deletes the field from the class",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.delete_field,
+            "num_arguments" : 2,
+            "skip_line" : False
+        }
+    ],
+    "move_up_field" : [
+        {
+            "usage" : "move_up_field <class_name> <field_name>",
+            "desc" : "moves field up one space in a list of fields",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.move_up_field,
+            "num_arguments" : 2,
+            "skip_line" : False
+        }
+    ],
+    "move_down_field" : [
+        {
+            "usage" : "move_down_field <class_name> <field_name>",
+            "desc" : "moves field down one space in a list of foe;ds",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.move_down_field,
+            "num_arguments" : 2,
+            "skip_line" : False
+        }
+    ],  
     "list_fields" : [
         {
             "usage" : "list_fields <class_name>",
             "desc" : "prints all of the fields for a given class",
             "command" : CLICommand,
             "function" : UMLModel.list_fields,
-            "num_arguments" : 1
-        }
-    ],
-    "list_relationships" : [
-        {
-            "usage" : "list_relationships",
-            "desc" : "prints out the classes that each class relate to",
-            "command" : CLICommand,
-            "function" : UMLModel.list_relationships,
-            "num_arguments" : 0
-        },
-        {
-            "usage" : "list_relationships <class_name>",
-            "desc" : "prints out the classes that a relate to class_name",
-            "command" : CLICommand,
-            "function" : UMLModel.list_relationships,
-            "num_arguments" : 1
+            "num_arguments" : 1,
+            "skip_line" : True
         }
     ],
      "create_method" : [
@@ -229,7 +129,8 @@ COMMANDS = {
             "desc" : "creates a method named <method_name> of type <method_type> and adds it to the given class \n <visibility> determines if the method is public/private,",
             "command" : UndoableCLICommand,
             "function" : UMLModel.create_method,
-            "num_arguments" : 4
+            "num_arguments" : 4,
+            "skip_line" : False
         }
     ],
     "rename_method" : [
@@ -238,7 +139,8 @@ COMMANDS = {
             "desc" : "renames the method <old_name> with the <new_name> for the class, <class_name>",
             "command" : UndoableCLICommand,
             "function" : UMLModel.rename_method,
-            "num_arguments" : 3
+            "num_arguments" : 3,
+            "skip_line" : False
         }
     ],
     "delete_method" : [
@@ -247,7 +149,8 @@ COMMANDS = {
             "desc" : "deletes the method from the class",
             "command" : UndoableCLICommand,
             "function" : UMLModel.delete_method,
-            "num_arguments" : 2
+            "num_arguments" : 2,
+            "skip_line" : False
         }
     ],
     "move_up_method" : [
@@ -256,7 +159,8 @@ COMMANDS = {
             "desc" : "moves method up one space in a list of methods",
             "command" : UndoableCLICommand,
             "function" : UMLModel.move_up_method,
-            "num_arguments" : 2
+            "num_arguments" : 2,
+            "skip_line" : False
         }
     ],
     "move_down_method" : [
@@ -265,7 +169,8 @@ COMMANDS = {
             "desc" : "moves method down one space in a list of methods",
             "command" : UndoableCLICommand,
             "function" : UMLModel.move_down_method,
-            "num_arguments" : 2
+            "num_arguments" : 2,
+            "skip_line" : False
         }
     ],
     "list_methods" : [
@@ -274,7 +179,132 @@ COMMANDS = {
             "desc" : "prints all of the methods for a given class",
             "command" : CLICommand,
             "function" : UMLModel.list_methods,
-            "num_arguments" : 1
+            "num_arguments" : 1,
+            "skip_line" : True
         }
-    ]
+    ],
+    "create_relationship" : [
+        {
+            "usage" : "create_relationship <relationship_type> <class1> <class2>",
+            "desc" : "creates a relationship between <class1> and <class2>\n<relationship_type> should be one of \n{inheritance, generalization, aggregation, composition}",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.create_relationship,
+            "num_arguments" : 3,
+            "skip_line" : False
+        }
+    ],
+    "delete_relationship" : [
+        {
+            "usage" : "delete_relationship <class1> <class2>",
+            "desc" : "removes the relationship between <class1> and <class2>",
+            "command" : UndoableCLICommand,
+            "function" : UMLModel.delete_relationship,
+            "num_arguments" : 2,
+            "skip_line" : False
+        }
+    ],
+    "move_up_relationship" : [
+       {
+            "usage" : "move_up_relationship <class_name1> <class_name2>",
+            "desc" : "moves relationship up one space in a list of the first specified class's relationships",
+            "function" : UMLModel.move_up_relationship,
+            "num_arguments" : 2,
+            "skip_line" : False
+       }
+    ],
+    "move_down_relationship" : [
+       {
+            "usage" : "move_down_relationship <class_name1> <class_name2>",
+            "desc" : "moves relationship down one space in a list of the first specified class's relationships",
+            "function" : UMLModel.move_down_relationship,
+            "num_arguments" : 2,
+            "skip_line" : False
+       }
+    ],
+    "list_relationships" : [
+        {
+            "usage" : "list_relationships",
+            "desc" : "prints out the classes that each class relate to",
+            "command" : CLICommand,
+            "function" : UMLModel.list_relationships,
+            "num_arguments" : 0,
+            "skip_line" : False
+        },
+        {
+            "usage" : "list_relationships <class_name>",
+            "desc" : "prints out the classes that a relate to class_name",
+            "command" : CLICommand,
+            "function" : UMLModel.list_relationships,
+            "num_arguments" : 1,
+            "skip_line" : True
+        }
+    ],
+    "save_model" : [
+        {
+            "usage" : "save_model <filename>",
+            "desc" : "saves the UMLModel into <filename> as JSON",
+            "command" : CLICommand,
+            "function" : UMLModel.save_model,
+            "num_arguments" : 1,
+            "skip_line" : False
+        }
+    ],
+    "load_model" : [
+        {
+            "usage" : "load_model <filename>",
+            "desc" : "loads the UMLModel from <filename> and uses it as the working UMLModel",
+            "command" : CLICommand,
+            "function" : UMLModel.load_model,
+            "num_arguments" : 1,
+            "skip_line" : True
+        }
+    ],
+    "undo" : [
+        {
+            "usage" : "undo", 
+            "desc" : "undoes the last undoable command (if there was one)",
+            "command" : CLICommand,
+            "function" : CLIEditor.undo,
+            "num_arguments" : 0,
+            "skip_line" : False
+        }
+    ],
+    "redo" : [
+        {
+            "usage" : "redo", 
+            "desc" : "redoes the last undo'd command (if there was one)",
+            "command" : CLICommand,
+            "function" : CLIEditor.redo,
+            "num_arguments" : 0,
+            "skip_line" : True
+        }
+    ],
+    "help" : [
+        {
+            "usage" : "help", 
+            "desc" : "lists all possible commands",
+            "command" : CLICommand,
+            "function" : CLIEditor.print_help_message,
+            "num_arguments" : 0,
+            "skip_line" : False
+        },
+        {
+            "usage" : "help <command_name>", 
+            "desc" : "lists the usage for <command_name>",
+            "command" : CLICommand,
+            "function" : CLIEditor.print_help_message,
+            "num_arguments" : 1,
+            "skip_line" : False
+        }
+    ],
+    "exit" : [
+        {
+            "usage" : "exit",
+            "desc" : "exits the program",
+            "command" : CLICommand,
+            "function" : CLIEditor.prompt_exit,
+            "num_arguments" : 0,
+            "skip_line" : False
+        }
+    ],
 }
