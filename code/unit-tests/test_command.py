fe8f178f64659c0457284104f6b6006f830016c2
Amy Burnett <16aburnett@gmail.com>
2020-11-22 23:40:33 -0500

    added tests for edit class gui

--- code/unit-tests/test_command.py
+++ code/unit-tests/test_command.py
@@ -63,3 +63,47 @@ class CreateClassGUICommandTest(unittest.TestCase):
         self.assertTrue(len(model.classes["class1"].fields) == 2)
 
 ##########################################################################
+
+class EditClassGUICommandTest(unittest.TestCase):
+
+    def test_execute(self):
+
+        # create model 
+        model_name = "myModel"
+        model = UMLModel()
+        model.create_class("class1")
+        model.save_model(model_name + ".json", directory=os.getcwd() + "/")
+
+        class_data = {
+        "filename" : model_name + ".json",
+        "directory" : os.getcwd() + "/",
+        "original_name" : "class1",
+        "class_name" : "class2",
+        "field_visibilities" : ["private", "private"],
+        "field_types" : ["string", "int"],
+        "field_names" : ["name", "age"],
+        "method_visibilities" : ["public"],
+        "method_types" : ["string"],
+        "method_names" : ["getName"],
+        "parameter_method_index" : [],
+        "parameter_types" : [],
+        "parameter_names" : [],
+        "relationship_types" : [],
+        "relationship_others" : []
+        }
+
+        cmd = EditClassGUICommand(model, class_data)
+
+        cmd.saveBackup()
+
+        status, msg = cmd.execute()
+
+        # ensure it passed
+        self.assertTrue(status)
+
+        # ensure components are in model
+        self.assertTrue("class2" in model.classes)
+        self.assertTrue(len(model.classes["class2"].fields) == 2)
+
+
+##########################################################################
